775c234c87ba26de96f2919a0789ef5d9f84318e
GodwinOwonam <godwin.owonamg5@gmail.com>
2024-09-18 19:40:40 +0530

    FEAT: Completed the basic table lesson

--- app/Livewire/ShowPosts.php
+++ app/Livewire/ShowPosts.php
@@ -3,8 +3,10 @@
 namespace App\Livewire;
 
 use App\Models\Post;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Posts')]
 class ShowPosts extends Component
 {
     public function delete(Post $post)
